eeff93a049e37c6b69c10d64e2758f9738d14434
= <=>
2022-10-11 18:20:56 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const mysql = require('./lazy_mysql');
+// const mysql = require('./active_mysql');
+
+const config = { maxConnection: 5 };
+
+const pool = mysql.createPool(config);
+console.log("Pool query: ",pool.query('SELECT * FROM user WHERE user_id = ? AND email = ?;', [1, "domingo@gamil.com"]))
+
+// get first connection, and release.
+const connection = pool.getConnection();
+console.log('First connection: ', connection)
+console.log(connection.query('SELECT * FROM user WHERE user_id = ? AND email = ?;', [1, "domingo@gamil.com"]))
+connection.release();
+
+// get a connection.
+const conn2 = pool.getConnection();
+console.log('Second connection: ', conn2);
+
+// get too much connection.
+console.log(pool.getConnection());
+console.log(pool.getConnection());
+console.log(pool.getConnection());
+console.log(pool.getConnection());
+console.log(pool.getConnection());
